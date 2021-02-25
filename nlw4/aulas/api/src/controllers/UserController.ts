0830103865bbc4f8e5a6b7d93f5830d9f439426b
Ikedaaaa <matheus-ikeda@hotmail.com>
2021-02-25 01:34:58 -0300

    Node - Segunda Aula: Banco de Dados, criação de migration e tabela users

---
+++ nlw4/aulas/api/src/controllers/UserController.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from "../models/User";
+
+class UserController{
+    async create(request: Request, response: Response){
+        const {name, email} = request.body;
+
+        const usersRepository = getRepository(User);
+
+        const userAlreadyExists = await usersRepository.findOne({
+            email
+        });
+
+        if(userAlreadyExists){
+            return response.status(400).json({
+                error: "User already exists!"
+            });
+        }
+
+        const user = usersRepository.create({
+            name,
+            email
+        });
+
+        await usersRepository.save(user);
+
+        return response.json(user);
+    }
+    async list(request: Request, response: Response){
+        const usersRepository = getRepository(User);
+
+        const allUsers = await usersRepository.query("SELECT * FROM users");
+
+        return response.json(allUsers);
+    }
+}
+
+export { UserController };
--- nlw4/aulas/api/src/server.ts
+++ nlw4/aulas/api/src/server.ts
@@ -1,11 +1,35 @@
-import express, { response } from 'express';
+import 'reflect-metadata';
+import express from 'express';
+import './database';
+import { router } from './routes';
 
 const app = express();
+
+app.use(express.json());
+app.use(router);
+
+app.listen(3333, () => console.log("Positivo e operante!"));
+
+
+
+
+
+
+
+
+
+
 
 /**
  * 1º Parâmetro: Rota (Recurso da API);
  * 2º Parâmetro: request, response
- */
+
+/*
+GET => Buscar
+POST => Salvar
+PUT => Alterar
+DELETE => Deletar
+PATCH => Alteração específica
 
 //http://localhost:3333/users
 app.get('/users', (request, response) => {
@@ -16,13 +40,5 @@ app.get('/users', (request, response) => {
 app.post('/', (request, response) => {
     return response.json({message: "Dados salvos, senhor(a)"})
 })
-
-app.listen(3333, () => console.log("Positivo e operante!"));
-
-/*
-GET => Buscar
-POST => Salvar
-PUT => Alterar
-DELETE => Deletar
-PATCH => Alteração específica
 */
+
