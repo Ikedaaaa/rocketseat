cd1f96a72927ac2faf9d4579973ca7f41c9bfcd1
Ikedaaaa <matheus-ikeda@hotmail.com>
2021-02-28 17:53:08 -0300

    React - Quarta Aula: Leveling, Regras de negócio e Notificação

--- nlw4/aulas/moveit-next/src/components/ChallengeBox.tsx
+++ nlw4/aulas/moveit-next/src/components/ChallengeBox.tsx
@@ -1,9 +1,21 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox(){
-    const {activeChallenge, resetChallenge} = useContext(ChallengesContext);
+    const {activeChallenge, resetChallenge, completeChallenge} = useContext(ChallengesContext);
+    const {resetCountdown} = useContext(CountdownContext);
+
+    function handleChallengesSucceeded(){
+        completeChallenge();
+        resetCountdown();
+    }
+
+    function handleChallengesFailed(){
+        resetChallenge();
+        resetCountdown();
+    }
 
     return (
         <div className={styles.challengeBoxContainer}>
@@ -16,12 +28,19 @@ export function ChallengeBox(){
                         <p>{activeChallenge.description}</p>
                     </main>
                     <footer>
-                        <button type="button" className={styles.challengeFailedButton} onClick={resetChallenge}>
+
+                        <button type="button"
+                        className={styles.challengeFailedButton}
+                        onClick={handleChallengesFailed}>
                             Falhei
                         </button>
-                        <button type="button" className={styles.challengeSucceededButton}>
+
+                        <button type="button"
+                        className={styles.challengeSucceededButton}
+                        onClick={handleChallengesSucceeded}>
                             Completei
                         </button>
+                        
                     </footer>
                 </div>
             ) : (
